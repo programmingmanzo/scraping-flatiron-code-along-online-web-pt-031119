d7327ed594b87bc0bfa1aeb261cbe4f7e84cd662
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-23 03:56:50 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,14 +3,18 @@ class Course
   attr_accessor :title, :schedule, :description
   
   @@all = [] 
+  
+  def initialize
+    @@all << self 
+  end 
  
  
   def self.all 
-    @@all << self 
+    @@all
   end 
   
   def self.reset_all 
-    @@all = []
+    @@all.clear 
   end 
   
   
